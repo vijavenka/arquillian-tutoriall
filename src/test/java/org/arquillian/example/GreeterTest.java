d777c1e9cfd2de5a2d8e725479f0ced143da3e4c
Vijay <vijayavenkatesh.vn@gmail.com>
2021-02-26 13:25:55 +0530

    Initial files

---
+++ src/test/java/org/arquillian/example/GreeterTest.java
@@ -0,0 +1,36 @@
+package org.arquillian.example;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+// http://arquillian.org/guides/getting_started/#create_a_component
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+    	JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+    	        .addClasses(Greeter.class, PhraseBuilder.class)
+    	        .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    	    System.out.println(jar.toString(true));
+    	    return jar;
+    }
+
+    @Inject
+    Greeter greeter;
+
+    @Test
+    public void should_create_greeting() {
+        Assert.assertEquals("Hello, Earthling!", greeter.createGreeting("Earthling"));
+        greeter.greet(System.out, "Earthling");
+    }
+}
